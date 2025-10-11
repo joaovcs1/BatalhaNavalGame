1be07480faf71287fdb3d8c9cdb0955cd4ce547e
Joao Cruz <seuemail>
2025-10-11 01:03:08 -0300

    preenche_frota v1

--- funcoes.py
+++ funcoes.py
@@ -10,3 +10,18 @@ def define_posicoes(linha, coluna, orientacao, tamanho):
             opcoes.append([linha,coluna+i])
 
     return opcoes
+
+def preenche_frota(dicio, navio, linha, coluna, orientacao, tamanho):
+    dicio[f'{navio}'] = define_posicoes(linha, coluna, orientacao, tamanho)
+
+    return dicio
+
+frota = {}
+nome_navio = 'navio-tanque'
+linha = 6
+coluna = 1
+orientacao = 'horizontal'
+tamanho = 3
+
+resultado = preenche_frota(frota, nome_navio, linha, coluna, orientacao, tamanho)
+print(resultado)
