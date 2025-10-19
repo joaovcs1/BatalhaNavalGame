63b39e4794a2397fae621da905283b8cbe4f4e12
loostabile <lorenzostabile10@gmail.coml>
2025-10-19 16:09:49 -0300

    completo ate ex 5

--- funcoes.py
+++ funcoes.py
@@ -19,3 +19,35 @@ def preenche_frota(dicio, navio, linha, coluna, orientacao, tamanho):
     dicio[navio].append(navio_cor)
 
     return dicio
+def faz_jogada (tabuleiro,linha,coluna):
+    if tabuleiro[linha][coluna] == 1:
+        tabuleiro[linha][coluna] = 'X'
+    else:
+        tabuleiro[linha][coluna] = '-'
+    return tabuleiro
+def posiciona_frota (dicio):
+    tabuleiro = []
+    for i in range(10):
+        tabuleiro.append([0]* 10)
+    for navios,posicoes in dicio.items():
+        for posicao in posicoes:
+            for cord in posicao:
+                linha = cord[0]
+                coluna = cord[1]
+            
+                tabuleiro[linha][coluna] = 1
+    return tabuleiro
+def afundados(dicio,tabuleiro):
+    conta = 0
+    for navios, posicoes in dicio.items():
+        for posicao in posicoes:
+            afundado = True
+            for cord in posicao:
+                linha = cord[0]
+                coluna = cord[1]
+                if tabuleiro[linha][coluna] != 'X':
+                    afundado = False      
+            if afundado == True:
+                conta += 1
+    return conta
+    
