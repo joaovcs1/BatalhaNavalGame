dee1a373b5840f6366cd3cbd5cd85e4489bb4d70
Joao Cruz <seuemail>
2025-10-24 23:51:59 -0300

    def monta_tabuleiros e inicio de confronto

--- programa.py
+++ programa.py
@@ -42,3 +42,70 @@ for navio, definicao in definicao_frota.items():
                 
             else:
                 print('Esta posição não está válida!')
+
+            
+frota_oponente = {
+    'porta-aviões': [
+        [[9, 1], [9, 2], [9, 3], [9, 4]]
+    ],
+    'navio-tanque': [
+        [[6, 0], [6, 1], [6, 2]],
+        [[4, 3], [5, 3], [6, 3]]
+    ],
+    'contratorpedeiro': [
+        [[1, 6], [1, 7]],
+        [[0, 5], [1, 5]],
+        [[3, 6], [3, 7]]
+    ],
+    'submarino': [
+        [[2, 7]],
+        [[0, 6]],
+        [[9, 7]],
+        [[7, 6]]
+    ]
+}
+
+tabuleiro_jogador = posiciona_frota(frota)
+tabuleiro_oponente = posiciona_frota(frota_oponente)
+
+total_navios_oponente = 0
+for lista_navios in frota_oponente.values():
+    total_navios_oponente += len(lista_navios)
+
+jogando = True
+posicoes_atacadas = [] 
+
+while jogando:
+    
+    print(monta_tabuleiros(tabuleiro_jogador, tabuleiro_oponente))
+
+    linha_valida = False
+    while not linha_valida:
+        linha_ataque = int(input('Linha de ataque: '))
+        if 0 <= linha_ataque <= 9:
+            linha_valida = True
+        else:
+            print('Linha inválida!')
+
+    coluna_valida = False
+    while not coluna_valida:
+        coluna_ataque = int(input('Coluna de ataque: '))
+        if 0 <= coluna_ataque <= 9:
+            coluna_valida = True
+        else:
+            print('Coluna inválida!')
+            
+    coordenada_ataque = [linha_ataque, coluna_ataque]
+    
+    if coordenada_ataque in posicoes_atacadas:
+        print(f'A posição linha {linha_ataque} e coluna {coluna_ataque} já foi informada anteriormente!')
+        continue
+    
+    posicoes_atacadas.append(coordenada_ataque)
+    tabuleiro_oponente = faz_jogada(tabuleiro_oponente, linha_ataque, coluna_ataque)
+
+    navios_afundados_oponente = afundados(frota_oponente, tabuleiro_oponente)
+    
+    if navios_afundados_oponente == total_navios_oponente:
+        print('Parabéns! Você derrubou todos os navios do seu oponente!')
+        jogando = False 
